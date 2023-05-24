69d88f2c0f0975830979b38c37899d073f44e1d8
E V I R A L <74889213+Teameviral@users.noreply.github.com>
2023-05-24 14:27:47 +0530

    Update withfingerprint.ino

--- Arduino Code/withfingerprint.ino
+++ Arduino Code/withfingerprint.ino
@@ -1,119 +1,112 @@
-#include <Adafruit_Fingerprint.h> // Library for fingerprint sensor
+#include <SoftwareSerial.h>
 
-Adafruit_Fingerprint finger = Adafruit_Fingerprint(&Serial1); // Create an instance of the fingerprint sensor
+SoftwareSerial bluetooth(10, 11); // Bluetooth module connected to pins 10 and 11
 
 String inputString = "";
 String command = "";
 String value = "";
-String gotstatus = "";
-String password = "write your password here"; // This is the password for opening and closing your door.
-                                              // You can set any password you like using digits and symbols.
+String password = "write your password here"; // Password for opening and closing the door
 boolean stringComplete = false;
+boolean authenticated = false;
+boolean fingerprintAdded = false;
 
-int motorPin1 = 10; // Pin 2 on L293D IC
-int motorPin2 = 11; // Pin 7 on L293D IC
+int motorPin1 = 2; // Pin 2 on L293D IC
+int motorPin2 = 3; // Pin 7 on L293D IC
 int motorEnablePin = 9; // Pin 1 on L293D IC
-int Speed = 10;
-
-// Data structure to store access log history
-struct AccessLog {
-  String timeStamp;
-  String status;
-};
-
-const int maxAccessLogs = 10; // Maximum number of access logs to store
-AccessLog accessLogs[maxAccessLogs]; // Array to store access logs
-int accessLogCount = 0; // Counter for the number of access logs
 
 void setup() {
-  // Start serial connections
-  Serial.begin(9600);  // Baud rate is 9600, must match with Bluetooth
-  Serial1.begin(57600); // Baud rate for fingerprint sensor, adjust according to your sensor
-  
-  inputString.reserve(50);  // Reserve 50 bytes in memory for string manipulation 
+  // Start serial connection
+  Serial.begin(9600);
+  bluetooth.begin(9600);
+
+  inputString.reserve(50);
   command.reserve(50);
   value.reserve(50);
 
   pinMode(motorPin1, OUTPUT);
   pinMode(motorPin2, OUTPUT);
   pinMode(motorEnablePin, OUTPUT);
-  pinMode(2, INPUT_PULLUP);
 
-  // Initialize fingerprint sensor
-  finger.begin(57600);
-  
-  if (finger.verifyPassword()) {
-    Serial.println("Fingerprint sensor found!");
-  } else {
-    Serial.println("Did not find fingerprint sensor :(");
-    while (1);
-  }
-  
-  finger.getTemplateCount();
-  Serial.print("Sensor contains "); 
-  Serial.print(finger.templateCount); 
-  Serial.println(" templates");
+  // Initialize Bluetooth connection
+  waitForConnection();
 }
 
 void loop() {
   if (stringComplete) {
-    int sensorVal = digitalRead(2);
-    delay(100);
-
-    if (inputString == "GETSTATUS\n") {
-      if (sensorVal == HIGH) {
-        gotstatus = "UNLOCKED";
-        Serial.println("UNLOCKED");
-      }
-      if (sensorVal == LOW) {
-        gotstatus = "LOCKED";
-        Serial.println("LOCKED");
-      }
-    }
-
-    int pos = inputString.indexOf('=');
-
-    if (pos > -1) {
-      command = inputString.substring(0, pos);
-      value = inputString.substring(pos + 1, inputString.length() - 1);
-
-      if (!password.compareTo(value) && (command == "OPEN") && digitalRead(2) == LOW) {
-        if (authenticateFingerprint()) {
-          openDoor();
-          logAccess("UNLOCKED");
-          Serial.println("OPENING");
-          delay(100);
-        } else {
-          Serial.println("Fingerprint authentication failed");
-          delay(100);
-        }
-      } else if (!password.compareTo(value) && (command == "CLOSE") && digitalRead(2) == HIGH) {
-        if (authenticateFingerprint()) {
-          closeDoor();
-          logAccess("LOCKED");
-          Serial.println("CLOSING");
-          delay(100);
-        } else {
-          Serial.println("Fingerprint authentication failed");
-          delay(100);
-        }
-      } else if (password.compareTo(value)) {
-        Serial.println("Wrong password");
-              delay(100);
-      }
-    }
-
+    command = inputString.substring(0, inputString.indexOf('='));
+    value = inputString.substring(inputString.indexOf('=') + 1, inputString.length() - 1);
     inputString = "";
     stringComplete = false;
+
+    if (command == "PASSWORD") {
+      if (authenticated) {
+        sendBluetoothMessage("ALREADY_AUTHENTICATED");
+      } else if (password == value) {
+        authenticated = true;
+        sendBluetoothMessage("AUTHENTICATION_SUCCESS");
+        sendBluetoothMessage("ADD_FINGERPRINT?");
+      } else {
+        sendBluetoothMessage("AUTHENTICATION_FAILURE");
+      }
+    } else if (command == "FINGERPRINT") {
+      if (!authenticated) {
+        sendBluetoothMessage("NOT_AUTHENTICATED");
+      } else if (value == "ADD") {
+        if (fingerprintAdded) {
+          sendBluetoothMessage("FINGERPRINT_ALREADY_ADDED");
+        } else {
+          fingerprintAdded = true;
+          sendBluetoothMessage("FINGERPRINT_ADDED");
+          sendBluetoothMessage("OPEN_DOOR?");
+        }
+      } else if (value == "REMOVE") {
+        if (fingerprintAdded) {
+          fingerprintAdded = false;
+          sendBluetoothMessage("FINGERPRINT_REMOVED");
+        } else {
+          sendBluetoothMessage("NO_FINGERPRINT_ADDED");
+        }
+      }
+    } else if (command == "OPEN") {
+      if (!authenticated) {
+        sendBluetoothMessage("NOT_AUTHENTICATED");
+      } else if (!fingerprintAdded) {
+        sendBluetoothMessage("NO_FINGERPRINT_ADDED");
+      } else if (password == value) {
+        sendBluetoothMessage("ACCESS_GRANTED");
+        openDoor();
+      } else {
+        sendBluetoothMessage("ACCESS_DENIED");
+      }
+    } else if (command == "CLOSE") {
+      if (!authenticated) {
+        sendBluetoothMessage("NOT_AUTHENTICATED");
+      } else if (password == value) {
+        sendBluetoothMessage("DOOR_CLOSED");
+        closeDoor();
+      } else {
+        sendBluetoothMessage("ACCESS_DENIED");
+      }
+    }
   }
 }
 
-void serialEvent() {
-  while (Serial.available()) {
-    char inChar = (char)Serial.read();
-    inputString += inChar;
+void sendBluetoothMessage(String message) {
+  bluetooth.println(message);
+}
 
-    if (inChar == '\n' || inChar == '\r') {
+void waitForConnection() {
+  while (!bluetooth.available()) {
+    delay(100);
+  }
+  bluetooth.println("CONNECTED");
+}
+
+void serialEvent() {
+  while (bluetooth.available()) {
+    char inChar = (char)bluetooth.read();
+    inputString += inChar;
+    if (inChar == '\n') {
       stringComplete = true;
     }
   }
@@ -124,8 +117,11 @@ void openDoor() {
   digitalWrite(motorPin2, LOW);
   digitalWrite(motorEnablePin, HIGH);
   delay(375);
-  digitalWrite(motorPin1, LOW);
-  digitalWrite(motorPin2, LOW);
+  digitalWrite(motorEnablePin, LOW);
+  delay(500);
+  digitalWrite(motorEnablePin, HIGH);
+  delay(1000);
+  digitalWrite(motorEnablePin, LOW);
 }
 
 void closeDoor() {
@@ -133,104 +129,5 @@ void closeDoor() {
   digitalWrite(motorPin2, HIGH);
   digitalWrite(motorEnablePin, HIGH);
   delay(375);
-  digitalWrite(motorPin1, LOW);
-  digitalWrite(motorPin2, LOW);
+  digitalWrite(motorEnablePin, LOW);
 }
-
-bool authenticateFingerprint() {
-  Serial.println("Place your finger on the sensor");
-  
-  int fingerprintID = finger.getImage();
-  
-  if (fingerprintID == FINGERPRINT_OK) {
-    Serial.println("Image taken");
-    fingerprintID = finger.image2Tz();
-    
-    if (fingerprintID == FINGERPRINT_OK) {
-      Serial.println("Remove your finger");
-      delay(2000);
-      Serial.println("Place the same finger again");
-      
-      fingerprintID = finger.getImage();
-      
-      if (fingerprintID == FINGERPRINT_OK) {
-        Serial.println("Image taken");
-        fingerprintID = finger.image2Tz(1);
-        
-        if (fingerprintID == FINGERPRINT_OK) {
-          Serial.println("Fingerprint captured");
-          fingerprintID = finger.fingerFastSearch();
-          
-          if (fingerprintID == FINGERPRINT_OK) {
-            Serial.println("Fingerprint matched");
-            return true;
-          } else {
-            Serial.println("Fingerprint not found");
-          }
-        } else {
-          Serial.println("Failed to convert image to template");
-        }
-      } else {
-        Serial.println("Failed to take second finger image");
-      }
-    } else {
-      Serial.println("Failed to convert image to template");
-    }
-  } else {
-    Serial.println("Failed to take finger image");
-  }
-  
-  return false;
-}
-
-void logAccess(String status) {
-  // Get current timestamp
-  String timeStamp = getTimeStamp();
-  
-  // Create new access log entry
-  AccessLog newLog;
-  newLog.timeStamp = timeStamp;
-  newLog.status = status;
-  
-  // Add the new log entry to the access logs array
-  accessLogs[accessLogCount] = newLog;
-  
-  // Increment access log count
-  accessLogCount++;
-  
-  // If the maximum number of logs is reached, start overwriting the oldest logs
-  if (accessLogCount >= maxAccessLogs) {
-    accessLogCount = 0;
-  }
-  
-  // Print the access log entry
-  Serial.print("Access Log - ");
-  Serial.print("Timestamp: ");
-  Serial.print(newLog.timeStamp);
-  Serial.print(", Status: ");
-  Serial.println(newLog.status);
-}
-
-String getTimeStamp() {
-  // Get current time and date
-  // Adjust according to your time zone and RTC library
-  
-  // Example using built-in millis() function
-  unsigned long currentTime = millis();
-  int seconds = currentTime / 1000;
-  int minutes = seconds / 60;
-  int hours = minutes / 60;
-  int days = hours / 24;
-  
-  String timeStamp = String(days) + "d " + String(hours % 24) + "h " + String(minutes % 60) + "m " + String(seconds % 60) + "s";
-  return timeStamp;
-}
-
-void setup() {
-  // Setup code goes here
-}
-
-void loop() {
-  // Loop code goes here
-}
-
